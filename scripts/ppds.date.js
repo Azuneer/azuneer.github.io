f0170357f955b9db2d2e6ad70a342cc7bc49bd3f
Azuneer <e.gadonnaud@gmail.com>
2022-11-21 06:56:32 +0100

    added beaats, to be changed later

---
+++ scripts/ppds.date.js
@@ -0,0 +1,30 @@
+/*
+ * Date class extention
+ * v1.1.2, 2021-03-01
+ * Paul Philippov, paul@themactep.com
+ * Homepage: https://themactep.com/beats/js
+ * License: WTFPL
+ */
+
+// Date.toInternetTime(n)
+// Converts time to Swatch Internet Time format.
+// n - the number of digits after decimal point.
+Date.prototype.toInternetTime = function (n) {
+  const BeatInSeconds = 86.4;
+  const seconds = this.getUTCSeconds();
+  const minutes = this.getUTCMinutes();
+  let hours = this.getUTCHours();
+  hours = (23 === hours) ? 0 : hours + 1;
+  const BielMeanTime = (hours * 60 + minutes) * 60 + seconds;
+  const beats = Math.abs(BielMeanTime / BeatInSeconds).toFixed(parseInt(n));
+  const length = (n > 0) ? 1 + n : 0;
+  return '@'.concat('000'.concat(beats).slice(beats.length - length));
+};
+
+// Date.getDayOfYear()
+// Returns the number of days from the beginning of year to the date.
+Date.prototype.getDayOfYear = function () {
+  const DayInSeconds = 86400000;
+  const BeginningOfYear = new Date(this.getFullYear(), 0, 1);
+  return Math.ceil((this - BeginningOfYear) / DayInSeconds);
+};
